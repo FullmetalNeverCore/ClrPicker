30433b6e9b81db81f6bc17f4448f828e180ac9bd
FullmetalNeverCore <catharasis@mail.ru>
2024-04-15 03:39:13 +0300

    FC

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ClrPickerWPF
+{
+
+
+
+
+    public partial class MainWindow : Window
+    {
+
+        private DispatcherTimer ttimer = new DispatcherTimer();
+        private back.ColorPicker clrpckr = new back.ColorPicker();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetTimer();
+
+
+        }
+
+        public void SetTimer()
+        {
+            ttimer.Interval = TimeSpan.FromMilliseconds(100);
+            ttimer.Tick += ClrUpdate;
+            ttimer.Start();
+        }
+
+
+        private void ClrUpdate(object sender,EventArgs e)
+        {
+            System.Windows.Media.Color color = clrpckr.getColor();
+            CLR.Fill = new SolidColorBrush(System.Windows.Media.Color.FromRgb(color.R,color.G,color.B));
+            HW.Text = $"RGB:{color.R},{color.G},{color.B}";
+        }
+
+    }
+}
---
+++ back/ColorPicker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClrPickerWPF.back
+{
+    public class ColorPicker
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(out POINT lpoint);
+
+        [DllImport("gdi32.dll")]
+        static extern uint GetPixel(IntPtr hdc, int nXPos, int nYPos);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        static extern int ReleaseDC(IntPtr hwnd, IntPtr hdc);
+
+        public System.Windows.Media.Color getColor()
+        {
+            POINT p;
+            IntPtr hdc = IntPtr.Zero;
+            if (!GetCursorPos(out p))
+            {
+                throw new InvalidOperationException("Was not able to obtain cursor position!");
+            }
+            try
+            {
+                hdc = GetDC(IntPtr.Zero);
+                uint pixel = GetPixel(hdc, p.X, p.Y);
+                ReleaseDC(IntPtr.Zero, hdc);
+                System.Windows.Media.Color color =
+                    System.Windows.Media.Color.FromRgb((byte)(pixel & 0x0000FF),
+                    (byte)((pixel & 0x00FF00) >> 8),
+                    (byte)((pixel & 0xFF0000) >> 16));
+                return color;
+            }
+            finally
+            {
+                if (hdc != IntPtr.Zero)
+                {
+                    ReleaseDC(IntPtr.Zero, hdc);
+                }
+            }
+        }
+
+    }
+
+}
